584c2526a89759845257947f6366d753e1d7f815
Charles Machalow <csm10495@gmail.com>
2021-03-30 20:34:12 -0700

    update readme

--- generate_docs.py
+++ generate_docs.py
@@ -3,10 +3,25 @@ script to create docs
 '''
 
 import os
+import pathlib
 import re
 import shutil
 import subprocess
 import sys
+
+def add_usage_info_to_readme():
+    '''
+    This is specific for errgrep. It will add output from errgrep --help to README.md
+    '''
+    MARKER='\n[CLI_OUTPUT_MARKER]::\n'
+
+    output = subprocess.check_output('errgrep --help', shell=True).decode()
+
+    readme = pathlib.Path('README.md').read_text()
+    pre, _, post = readme.split(MARKER)
+    full_text = pre + MARKER + '\n```\n' + output + '\n```\n' + MARKER + post
+    full_text = full_text.replace('\r\n', '\n')
+    pathlib.Path('README.md').write_text(full_text)
 
 if __name__ == '__main__':
     assert subprocess.call([
@@ -37,3 +52,6 @@ if __name__ == '__main__':
     # remove extra dir nesting and move back to docs/
     shutil.move('docs_tmp/errgrep', 'docs')
     os.rmdir('docs_tmp')
+
+    # special for errgrep
+    add_usage_info_to_readme()
